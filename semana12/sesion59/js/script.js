e9ae3c1a40eec2b97b6dd71a2f1f784ad3f29a65
Cinthiapiera <cinthia.maldonado@upch.pe>
2024-09-30 22:11:40 -0500

    sesion 59

---
+++ semana12/sesion59/js/script.js
@@ -0,0 +1,8 @@
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+//Inicializar Popovers
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
